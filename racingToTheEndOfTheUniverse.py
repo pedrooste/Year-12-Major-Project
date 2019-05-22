01b1f2afcc868fa350e66c2a6efc1208f40f97d9
Pedro mate <36180116+pedrooste@users.noreply.github.com>
2019-05-22 18:29:18 +1000

    Racing to the end of the universe
    This is the first update of sprint 2:
    Changes:
    - have changed all of the button modules to classes
    - created a loop system which flicks between the different game pages
    
    Needing to do:
    create basic instruction screen
    create game
    
    
    errors:
    - worried about a future error that may arrise having the main game play loop inside a module. More specifically how postions will be updated as the game plays.

--- racingToTheEndOfTheUniverse.py
+++ racingToTheEndOfTheUniverse.py
@@ -33,100 +33,69 @@ lightGreen = (0, 249, 54)
 blue = (5, 0, 165)
 lightBlue = (8, 0, 255)
 
+#creates objects to be used later on in classes
+playB = button(green,lightGreen,50,500,150,50,"PLAY",34,black,"play")
+quitB = button(red,lightRed,600,500,150,50,"QUIT",34,black,"quit")
+instructionsB = button(blue,lightBlue,300,500,200,50,"INSTRUCTIONS",34,black,"instructions")
+introB = button(red,lightRed,600,500,150,50,"BACK",34,black,"intro")
 
 
-def introscreen():
+def introscreen(playScreen):
     """Displays the intro screen
     Made up of a background, buttons and text
 
     Args:
-        arg1: none
+        playScreen: determines which screen to be displayed
 
     Returns:
-        description of the stuff that is returned by the function.
+        playScreen: determines which screen to be displayed
 
     Raises:
         AnError: An error occurred running this function.
     """
     screen.fill(white) #fills the screen with a background colour
-    button("PLAY",50,500,150,50,green,lightGreen,34,"play")
-    button("INSTRUCTIONS",300,500,200,50,blue,lightBlue,34,"instructions")
-    button("QUIT",600,500,150,50,red,lightRed,34,"quit")
-    rTxt("Racing to the end of the universe",400,50,48)
-    
-    
+    #draws the play, quit and instruction button
+    playScreen = playB.draw(screen, playScreen) 
+    playScreen =quitB.draw(screen, playScreen)
+    playScreen =instructionsB.draw(screen, playScreen)
+    return playScreen
 
-
-    pass
-
-def button(txt,x,y,l,h,colour,Hcolour,txtSize,action = None):
-    """Creates buttons and makes them interactive
-    Takes arguments and creates buttons to be displayed on the pygame screen, mouse position is then check to see if a button is highlighted or clicked.
-    Once clicked it will be sent to a different subroutine
+def playGame(playScreen):
+    """Displays the game screen
+    this will then reference to other classes
 
     Args:
-        txt: text to be displayed within the button
-        x: x value the button will be placed
-        y: y value the button will be placed
-        l: length of the button
-        h: height of the button
-        colour: natural colour displayed on the screen
-        Hcolour: colour displayed when the button is highlighted
-        txtSize: size of text in the button
-
+        playScreen: determines which screen to be displayed
     Returns:
+        playScreen: determines which screen to be displayed
 
     Raises:
         AnError: An error occurred running this function.
     """
-    mouse= P.mouse.get_pos() #gets X and Y of mouse position
-    click= P.mouse.get_pressed() #gets postion of mouse when clicked
-    
-    if x+l>mouse[0]>x and y+h>mouse[1]>y: #asks if the mouse is in the region where the button is located.
-        P.draw.rect(screen,Hcolour,(x,y,l,h)) #draws a rectangle with the highlighted colour if mouse is on it
-        if click[0]==1 and action!=None: #asks if the button has been clicked when it is within the reigion and if action is doing nothing
-            if action=="play": #determines which action to fufil
-                print('play button was clicked')
-            elif action =="instructions":
-                print('instruction button was clicked')
-            elif action =="quit":
-                P.quit()
-                quit()
-    else:
-        P.draw.rect(screen,colour,(x,y,l,h)) #draws a rectangle with the normal colour if mouse isnt on it
-    x1,y1 = ((x+(l/2)),(y+(h/2))) #sets coordinates for text using the length and height of the button as this cannot be done inside the Rtxt module
-    rTxt(txt,x1,y1,txtSize) #referes to the rTxt module to render the text inside the button
-    
-            
-    
-    pass
+    screen.fill(yellow) #fills the screen with a background colour
+    playScreen = introB.draw(screen,playScreen) #draws the back button which only will be used to go back to the intro screen
 
-def rTxt(msg,x,y,size):
-    """Renders and blits text
-    This module will be used whenever you would like to render text into the pygame window
+    return playScreen
+
+def instructionScreen(playScreen):
+    """Displays the instructions screen
+    this will then reference to other classes
 
     Args:
-        msg: text to be displayed
-        x: x value the button will be placed
-        y: y value the button will be placed
-        l: length of the button
-        h: height of the button
-        size: size of text
-
+        playScreen: determines which screen to be displayed
     Returns:
-        description of the stuff that is returned by the function.
+        playScreen: determines which screen to be displayed
 
     Raises:
         AnError: An error occurred running this function.
     """
-    
-    font = P.font.SysFont("comic sans",size) #creates a font for the render function to use
-    text = font.render(msg,True,black) #creates a text for the blit function to use
-    x,y = ((x- (text.get_rect().w/2)),(y- (text.get_rect().h/2))) #centres the text depening on the length and height of the text
-    screen.blit(text,(x,y))
-    
-    
-play = True   
+    screen.fill(black) #fills the screen with a background colour
+    playScreen = introB.draw(screen,playScreen)
+
+    return playScreen
+
+play = True
+playScreen = "intro" #intialises what screen to start on
 # game loop - runs loopRate times a second!
 while play:  # game loop - note:  everything in this loop is indented one tab
 
@@ -134,8 +103,16 @@ while play:  # game loop - note:  everything in this loop is indented one tab
         if event.type == P.QUIT:  # tests if window's X (close) has been clicked
             play = False  # causes exit of game loop
         
-        # your code starts here #
-        introscreen() #displays the intro screen
+        if playScreen == "intro": #this checks what screen to display by using a variable called playScreen
+            playScreen = introscreen(playScreen) #displays the intro screen
+        elif playScreen == "play":
+            playScreen = playGame(playScreen) #displays the play screen
+        elif playScreen == "instructions":
+            playScreen = instructionScreen(playScreen) #displays the instructions screen
+            
+        else:
+            playScreen = "intro"
+            print("there has been an error with playScreen, reverted to intro")
 
         if event.type == P.MOUSEBUTTONDOWN: #includes touching screen
             # change this to do something if user clicks mouse
