78daa54f884ab6649e125c2158ea8bc167c04dcc
Pedro mate <36180116+pedrooste@users.noreply.github.com>
2019-06-07 17:42:33 +1000

    Racing to the end of the universe
    End of sprint 4
    
    - Fixed previous issue of menu button and X not clicking, solved by placing the Menu button check above the letter check ( as this takes all inputs at the time). X click was checked when all presses were checked.
    - Fixed problem with the daily save. The issue was that the program was not reading the data into lists properly, i fixed this by only reading one line as all of the dates will be the same reguardless.
    - Added screen to all of the classes as a self. This makes it less cluttered when calling methods later on.
    
    End of sprint 4!

--- racingToTheEndOfTheUniverse.py
+++ racingToTheEndOfTheUniverse.py
@@ -38,34 +38,34 @@ lightBlue = (8, 0, 255)
 grey = (92, 98, 112)
 
 #creates objects to be used later on in button class
-playB = button(green,lightGreen,150,50,"PLAY",34,black,"play")
-highscoreB = button(red,lightRed,200,50,"HIGHSCORES",34,black,"highscore")
-instructionsB = button(blue,lightBlue,200,50,"INSTRUCTIONS",34,black,"instructions")
-introB = button(red,lightRed,150,50,"BACK",34,black,"intro")
-mainMenuB = button(red,lightRed,200,50,"MAIN MENU",34,black,'mainMenu')
-saveB = button(blue,lightBlue,200,50,"SAVE",34,black,'save')
-todayB = button(blue,lightBlue,150,50,"TODAYS",34,black,'today')
-overallB = button(blue,lightBlue,150,50,"OVERALL",34,black,'overall')
+playB = button(screen,green,lightGreen,150,50,"PLAY",34,black,"play")
+highscoreB = button(screen,red,lightRed,200,50,"HIGHSCORES",34,black,"highscore")
+instructionsB = button(screen,blue,lightBlue,200,50,"INSTRUCTIONS",34,black,"instructions")
+introB = button(screen,red,lightRed,150,50,"BACK",34,black,"intro")
+mainMenuB = button(screen,red,lightRed,200,50,"MAIN MENU",34,black,'mainMenu')
+saveB = button(screen,blue,lightBlue,200,50,"SAVE",34,black,'save')
+todayB = button(screen,blue,lightBlue,150,50,"TODAYS",34,black,'today')
+overallB = button(screen,blue,lightBlue,150,50,"OVERALL",34,black,'overall')
 
 #creates objects to be used later on in arrowButton class
-leftB = arrowButton(yellow,lightYellow,100,50,"left")
-rightB = arrowButton(yellow,lightYellow,100,50,"right")
+leftB = arrowButton(screen,yellow,lightYellow,100,50,"left")
+rightB = arrowButton(screen,yellow,lightYellow,100,50,"right")
 
 #creates objects to be used later in player class
-mcar = player(red,green,50,50) #main player car
+mcar = player(screen,red,green,50,50) #main player car
 
 #creates the enemy cars to be used later on
-Ecar0 = Ecar(white,5,-50,50,50)
-Ecar1 = Ecar(black,5,-225,50,100)
-Ecar2 = Ecar(lightBlue,5,-300,50,50)
-Ecar3 = Ecar(blue,5,-370,50,50)
-Ecar4 = Ecar(yellow,5,-440,50,50)
-Ecar5 = Ecar(lightRed,5,-510,50,50)
-Ecar6 = Ecar(red,5,-580,50,50)
+Ecar0 = Ecar(screen,white,5,-50,50,50)
+Ecar1 = Ecar(screen,black,5,-225,50,100)
+Ecar2 = Ecar(screen,lightBlue,5,-300,50,50)
+Ecar3 = Ecar(screen,blue,5,-370,50,50)
+Ecar4 = Ecar(screen,yellow,5,-440,50,50)
+Ecar5 = Ecar(screen,lightRed,5,-510,50,50)
+Ecar6 = Ecar(screen,red,5,-580,50,50)
 
 #creates two objects of Highscores which will be refered to acess methods ass well as printing scores
-HST = highscore('Todays Highscores')
-HS = highscore('Highscores')
+HST = highscore(screen,'Todays Highscores')
+HS = highscore(screen,'Highscores')
 
 
 #creating a dictionary that referes to each of the enemy car objects, this is to make it easier to call them later
@@ -117,9 +117,9 @@ class game():
         screen.fill(white) #fills the screen with a background colour
         rTxt(screen,"Racing to the end of the universe",400,50,48,black)
         #draws the play, quit and instruction button
-        self.playScreen = playB.draw(screen,50,500, self.playScreen) 
-        self.playScreen =highscoreB.draw(screen,550,500, self.playScreen)
-        self.playScreen =instructionsB.draw(screen,275,500, self.playScreen)
+        self.playScreen = playB.draw(50,500, self.playScreen) 
+        self.playScreen =highscoreB.draw(550,500, self.playScreen)
+        self.playScreen =instructionsB.draw(275,500, self.playScreen)
         
     def playGame(self):
         """Displays the game screen
@@ -133,8 +133,8 @@ class game():
         
         oldCarX = self.carX #creates a temporary variable which will be checked later on to see if the x postion has been changed
         #draws left and right buttons
-        self.carX = leftB.draw(screen,50,500,self.carX)
-        self.carX = rightB.draw(screen,650,500,self.carX)
+        self.carX = leftB.draw(50,500,self.carX)
+        self.carX = rightB.draw(650,500,self.carX)
         
         if oldCarX == self.carX: #if the x postion has been changed then movement will be true. If movement is true then the rectangle will be draw a different colour
             movement = False
@@ -144,16 +144,16 @@ class game():
         difficulty = checkScore(self.score) #checks the difficulty in order to determine how many cars to deply
         
         for i in range (0,difficulty): #deploys cars according to how hard the difficutly is
-            self.score = enemyCarDict[i].draw(screen,self.score)
+            self.score = enemyCarDict[i].draw(self.score)
             
         
-        mcar.draw(screen,self.carX,movement) #draws the main player rectangle car
+        mcar.draw(self.carX,movement) #draws the main player rectangle car
         
 
 
 
         rTxt(screen,("Score: "+str(self.score)),100,50,48,black)    
-        self.playScreen = introB.draw(screen,600,50,self.playScreen) #draws the back button which only will be used to go back to the intro screen
+        self.playScreen = introB.draw(600,50,self.playScreen) #draws the back button which only will be used to go back to the intro screen
         
         for i in range (0,difficulty): #checks all the cars that are depolyed
                 crash = enemyCarDict[i].checkHit(self.carX) #checks if the car hits the enemy car
@@ -171,25 +171,25 @@ class game():
         """
         screen.fill(black) #fills the screen with a background colour
         rTxt(screen,"Instructions",400,50,48,white)
-        self.playScreen = introB.draw(screen,600,50, self.playScreen) #draws the back button for the intro screen
+        self.playScreen = introB.draw(600,50, self.playScreen) #draws the back button for the intro screen
     
     def highscoreScreen(self):
         """Displays the highscore screen
         this will then reference to other classes
         """
         screen.fill(white) #fills the screen with a background colour
-        self.playScreen = introB.draw(screen,600,50, self.playScreen) #draws the back button for the intro screen
+        self.playScreen = introB.draw(600,50, self.playScreen) #draws the back button for the intro screen
 
             
         if self.today == True:
-            press = todayB.draw(screen,600,500, self.playScreen)
+            press = todayB.draw(600,500, self.playScreen)
             rTxt(screen,"Overall Highscores",400,50,48,black)
-            HS.printHighscore(screen,black)
+            HS.printHighscore(black)
             
         if self.today == False:
-            press = overallB.draw(screen,600,500, self.playScreen)
+            press = overallB.draw(600,500, self.playScreen)
             rTxt(screen,"Todays Highscores",400,50,48,black)
-            HST.printHighscore(screen,black)
+            HST.printHighscore(black)
 
         if press == True:
             self.today = False
@@ -207,27 +207,31 @@ class game():
         
         rTxt(screen,"You crashed!",400,100,48,black) #displays text saying you crashed
         rTxt(screen,("Score: "+str(self.score)),400,200,48,black) #displays your score
+        self.playScreen = mainMenuB.draw(150,450,self.playScreen) #draws a main menu button
         
         if self.saved == False:
             rTxt(screen,"Name: ",200,275,48,black) #displays text
             P.draw.rect(screen,black,(300,250,300,50),5) #border for name input
-            save = saveB.draw(screen,450,450,self.playScreen) #draws the save button
+            save = saveB.draw(450,450,self.playScreen) #draws the save button
             
             
             for event in P.event.get(): #gets any events from the user
-                if event.type == P.KEYDOWN: #checks if the event is a key press
+                if event.type == P.QUIT:  # tests if window's X (close) has been clicked
+                    P.quit()   # stops the game engine
+                elif event.type == P.KEYDOWN: #checks if the event is a key press
                     if event.key == P.K_BACKSPACE: #If the event is a backspace it will take away a character from the string
                         self.name = self.name[0:-1]
-                    elif event.key == P.K_TAB:
+                    elif event.key == P.K_TAB or event.key == P.K_RETURN:
                         pass #cant put a tab within the name as this is what seperates variables
                     else:
                         if len(self.name) < 10: #limit of characters is 10
                             self.name += event.unicode #adds the letter or symbol to the name
+        
                         
 
             rTxt(screen,self.name,400,275,48,black) #draws the name
         
-        self.playScreen = mainMenuB.draw(screen,150,450,self.playScreen) #draws a main menu button
+        
         if self.playScreen == "intro": #if the palyer wants to go to the main menu, positions must be reset
             self.carX = 375 #car reset positon
             self.score = 0 # resets the score
